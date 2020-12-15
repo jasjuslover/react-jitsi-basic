75107b1d7f5d0838e03dcc7442ec043bd0d916f3
Husni Kamal <husnikamal3@gmail.com>
2020-12-15 17:20:59 +0700

    bismillah, initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  const containerStyle = {
+    width: "800px",
+    height: "400px",
+  };
+
+  const jitsiContainerStyle = {
+    display: loading ? "none" : "block",
+    width: "100%",
+    height: "100%",
+  };
+
+  const startMeeting = () => {
+    const domain = "meet.jit.si";
+    const options = {
+      roomName: "1beb4680",
+      height: 400,
+      parentNode: document.getElementById("jitsi-container"),
+    };
+    const api = new window.JitsiMeetExternalAPI(domain, options);
+    api.addEventListener("videoConferenceJoined", () => {
+      console.log("Local User Joined");
+      setLoading(false);
+      api.executeCommand("displayName", "MyName");
+    });
+  };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://meet.jit.si/external_api.js";
+    // script.onload = () => startMeeting();
+    document.body.appendChild(script);
+
+    // if (window.JitsiMeetExternalAPI) startMeeting();
+    // else alert("Jitsi Meet API script not loaded");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={containerStyle}>
+        <div id="jitsi-container" style={jitsiContainerStyle} />
+      </div>
     </div>
   );
 }
