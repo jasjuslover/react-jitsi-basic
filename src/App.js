279d8608033828cc2c25dc04219e00884759b0b6
Husni Kamal <husnikamal3@gmail.com>
2020-12-18 11:06:23 +0700

    custom control jitsi basic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+/**
+ *
+ * Interviewer
+ * ["microphone", "camera", "recording", "fullscreen", "hangup"]
+ *
+ * Candidate
+ * ["microphone", "camera", "recording", "fullscreen"]
+ *
+ */
+
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [counter, setCounter] = useState(null);
+  const [isFinished, setIsFinished] = useState(false);
 
   const containerStyle = {
     width: "800px",
     height: "400px",
+    position: "relative",
+    alignSelf: "center",
   };
 
   const jitsiContainerStyle = {
-    display: loading ? "none" : "block",
+    display: "block",
     width: "100%",
     height: "100%",
+    zIndex: 2,
+  };
+
+  const timerStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    background: "black",
+    zIndex: 3,
+    width: "100%",
+    height: "100%",
+  };
+
+  const timerChildStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    height: "100%",
+    fontSize: 24,
+    color: "white",
+  };
+
+  const finishedConferenceCallStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    background: "black",
+    zIndex: 3,
+    width: "100%",
+    height: "100%",
+  };
+
+  const finishedConferenceChildStyle = {
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    fontSize: 24,
+    color: "white",
   };
 
   const startMeeting = () => {
@@ -21,29 +75,75 @@ function App() {
       roomName: "1beb4680",
       height: 400,
       parentNode: document.getElementById("jitsi-container"),
+      userInfo: {
+        email: "husnikamal3@gmail.com",
+        displayName: "John Doe",
+      },
+      configOverwrite: {
+        prejoinPageEnabled: false,
+      },
+      interfaceConfigOverwrite: {
+        TOOLBAR_BUTTONS: ["microphone", "camera", "recording", "fullscreen", "hangup"],
+      },
     };
     const api = new window.JitsiMeetExternalAPI(domain, options);
     api.addEventListener("videoConferenceJoined", () => {
       console.log("Local User Joined");
-      setLoading(false);
+      // setLoading(false);
       api.executeCommand("displayName", "MyName");
+      api.executeCommand("toggleLobby", false);
+    });
+
+    api.addEventListener("participantLeft", () => {
+      console.log("huwiw participantLeft");
+      setIsFinished(true);
+      api.dispose();
     });
   };
 
-  useEffect(() => {
+  const embedJitsi = () => {
     const script = document.createElement("script");
     script.src = "https://meet.jit.si/external_api.js";
-    // script.onload = () => startMeeting();
+    script.onload = () => startMeeting();
     document.body.appendChild(script);
+  };
 
-    // if (window.JitsiMeetExternalAPI) startMeeting();
-    // else alert("Jitsi Meet API script not loaded");
+  useEffect(() => {
+    setTimeout(() => {
+      embedJitsi();
+    }, 4000);
+
+    setCounter(5);
   }, []);
+
+  useEffect(() => {
+    if (counter === 0) {
+      setCounter(null);
+    }
+
+    if (!counter) return;
+
+    const intervalId = setInterval(() => {
+      setCounter(counter - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [counter]);
 
   return (
     <div className="App">
       <div style={containerStyle}>
         <div id="jitsi-container" style={jitsiContainerStyle} />
+        {counter > 0 && (
+          <div style={timerStyle}>
+            <div style={timerChildStyle}>{counter}</div>
+          </div>
+        )}
+        {isFinished && (
+          <div style={finishedConferenceCallStyle}>
+            <div style={finishedConferenceChildStyle}>Conference Finished</div>
+          </div>
+        )}
       </div>
     </div>
   );
